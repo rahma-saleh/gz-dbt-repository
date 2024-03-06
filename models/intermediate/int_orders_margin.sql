cdcbfb173b7e05b67cf098cf04725d6dabe79f81
rahma saleh <rs.alatwi@gmail.com>
2024-03-06 13:49:33 +0000

    save

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -5,6 +5,6 @@ SELECT
   ROUND(SUM(quantity),2)as quantity
   ROUND(SUM(purchase_cost),2) as purchase_cos
   ROUND(SUM(margin),2) as margin
-FROM {{ref("int_sales_margin")}}
+FROM { {ref("int_sales_margin")} }
 GROUP BY orders_id,date_date
 ORDER BY orders_id DESC
