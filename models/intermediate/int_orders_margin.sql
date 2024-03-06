32f08f1eb2df7d3592f5a4e4c017d4efde628e18
rahma saleh <rs.alatwi@gmail.com>
2024-03-06 13:28:09 +0000

    save

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,10 @@
-SELECT
-  orders_id,
-  date_date,
-  ROUND(SUM(revenue),2) as revenue,
-  ROUND(SUM(quantity),2) as quantity,
-  ROUND(SUM(purchase_cost),2) as purchase_cost,
-  ROUND(SUM(margin),2) as margin
-FROM {{ ref("int_sales_margin") }}
-GROUP BY orders_id,date_date
-ORDER BY orders_id DESC
+select
+    orders_id,
+    max(date_date) as date_date,
+    round(sum(revenue), 2) as revenue,
+    round(sum(quantity), 2) as quantity,
+    round(sum(purchase_cost), 2) as purchase_cost,
+    round(sum(margin), 2) as margin
+from {{ ref("int_sales_margin") }}
+group by orders_id
+order by orders_id desc
