da2126edc58e52f25d1af58f98f74458537bad8a
rahma saleh <rs.alatwi@gmail.com>
2024-03-06 16:50:45 +0000

    save

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,11 @@
+select
+    products_id,
+    date_date,
+    orders_id,
+    revenue,
+    quantity,
+    cast(purchase_price as float64),
+    round(s.quantity * cast(p.purchase_price as float64), 2) as purchase_cost,
+    s.revenue - round(s.quantity * cast(p.purchase_price as float64), 2) as margin
+from {{ ref("stg_raw__sales") }} s
+left join {{ ref("stg_raw__product") }} p using (products_id)
