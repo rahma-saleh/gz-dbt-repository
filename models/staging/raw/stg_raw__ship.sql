765d80996bd4fbe6f50c98d2057782dadedd64e8
rahma saleh <rs.alatwi@gmail.com>
2024-03-06 15:09:01 +0000

    save

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -2,7 +2,7 @@ with
 
     source as (select * from {{ source("raw", "ship") }}),
 
-    renamed as (select orders_id, shipping_fee, log_cost, ship_cost from source)
+    renamed as (select orders_id, shipping_fee, logcost, ship_cost from source)
 
 select *
 from renamed
