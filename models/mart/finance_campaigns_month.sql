c30d07a0522faef8658db3f7fd329dc0d7f45c46
rahma saleh <rs.alatwi@gmail.com>
2024-03-30 01:31:16 +0000

    save

--- models/mart/finance_campaigns_month.sql
+++ models/mart/finance_campaigns_month.sql
@@ -1,20 +1,19 @@
-SELECT 
-  date_trunc(date_date, MONTH) AS datemonth, 
-  SUM(operational_margin - ads_cost) AS ads_margin,
-  ROUND(SUM(average_basket),2) AS average_basket,
-  SUM(operational_margin) AS operational_margin,
-  SUM(ads_cost) AS ads_cost,
-  SUM(ads_impression) AS ads_impression,
-  SUM(ads_clicks) AS ads_clicks,
-  SUM(quantity) AS quantity,
-  SUM(revenue) AS revenue,
-  SUM(purchase_cost) AS purchase_cost,
-  SUM(margin) AS margin,
-  SUM(shipping_fee) AS shipping_fee,
-  SUM(logcost) AS logcost,
-  SUM(ship_cost) AS ship_cost,
-FROM {{ ref('int_campaigns_day') }}
-FULL OUTER JOIN {{ ref('finance_days') }} 
-  USING (date_date)
-GROUP BY datemonth
-ORDER BY datemonth desc
+select
+    date_trunc(date_date, month) as datemonth,
+    sum(operational_margin - ads_cost) as ads_margin,
+    round(sum(average_basket), 2) as average_basket,
+    sum(operational_margin) as operational_margin,
+    sum(cast(ads_cost as float64)) as ads_cost,
+    sum(ads_impression) as ads_impression,
+    sum(ads_clicks) as ads_clicks,
+    sum(quantity) as quantity,
+    sum(revenue) as revenue,
+    sum(purchase_cost) as purchase_cost,
+    sum(margin) as margin,
+    sum(shipping_fee) as shipping_fee,
+    sum(logcost) as logcost,
+    sum(cast(ship_cost as float64)) as ship_cost,
+from {{ ref("int_campaigns_day") }}
+full outer join {{ ref("finance_days") }} using (date_date)
+group by datemonth
+order by datemonth desc
