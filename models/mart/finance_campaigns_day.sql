a98fda5d168fdee8ace21dd89f70b89cfd1f9630
rahma saleh <rs.alatwi@gmail.com>
2024-03-30 01:28:54 +0000

    save

--- models/mart/finance_campaigns_day.sql
+++ models/mart/finance_campaigns_day.sql
@@ -1,19 +1,18 @@
-SELECT 
-  date_date,
-  operational_margin - ads_cost AS ads_margin,
-  ROUND(average_basket,2) AS average_basket
-  operational_margin,
-  ads_cost,
-  ads_impressionn,
-  ads_clicks,
-  quantity,
-  revenue,
-  purchase_cost,
-  margin,
-  shipping_fee,
-  logcost,
-  ship_cost
-FROM {{ ref('int_campaigns_day') }}
-FULL OUTER JOIN {{ ref('finance_days') }} 
-  USING (date_date)
-ORDER BY date_date DESC
+select
+    date_date,
+    operational_margin - ads_cost as ads_margin,
+    round(average_basket, 2) as average_basket
+    operational_margin,
+    ads_cost,
+    ads_impressionn,
+    ads_clicks,
+    quantity,
+    revenue,
+    purchase_cost,
+    margin,
+    shipping_fee,
+    logcost,
+    ship_cost
+from {{ ref("int_campaigns_day") }}
+full outer join {{ ref("finance_days") }} using (date_date)
+order by date_date desc
